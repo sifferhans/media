937251ea89bc66f77908863bca03c25e9a742999
Carsten Jacobsen <car.jacobsen@gmail.com>
2022-06-19 11:29:51 +0200

    feat(thumbnail): add download function

--- src/Thumbnail.vue
+++ src/Thumbnail.vue
@@ -9,8 +9,18 @@
       :style="style"
     >
     </component>
-    <div class="kvass-media-thumbnail--delete" @click="$emit('delete')">
-      <FontAwesomeIcon icon="trash"></FontAwesomeIcon>
+    <div class="kvass-media-thumbnail__actions">
+      <div class="kvass-media-thumbnail__delete" @click="$emit('delete')">
+        <FontAwesomeIcon icon="trash" />
+      </div>
+      <component
+        :is="download.component"
+        class="kvass-media-thumbnail__download"
+        v-bind="download.props"
+        v-on="download.listeners"
+      >
+        <FontAwesomeIcon icon="arrow-alt-circle-down" />
+      </component>
     </div>
   </div>
 </template>
@@ -28,12 +38,46 @@ export default {
     },
     disabled: {},
   },
-
   computed: {
     style() {
       return {
         width: this.size,
         height: this.size,
+      }
+    },
+    download() {
+      if (this.value.type.startsWith('image/'))
+        return {
+          component: 'div',
+          props: {},
+          listeners: {
+            click: () => {
+              fetch(this.value.url)
+                .then((res) => res.blob())
+                .then((blob) => {
+                  const blobURL = URL.createObjectURL(blob)
+                  const a = document.createElement('a')
+                  a.href = blobURL
+                  a.style = 'display: none'
+                  a.download = this.value.name
+
+                  document.body.appendChild(a)
+                  a.click()
+
+                  document.body.removeChild(a)
+                })
+            },
+          },
+        }
+
+      return {
+        component: 'a',
+        props: {
+          href: this.value.url,
+          download: this.value.name,
+          target: '_blank',
+        },
+        listeners: {},
       }
     },
   },
@@ -58,32 +102,50 @@ export default {
     }
   }
 
-  &--delete {
-    $size: 25px;
-    height: $size;
-    width: $size;
-    right: -$size/5;
-    top: -$size/5;
-    position: absolute;
-    z-index: 2;
-    color: white;
+  &__actions {
     display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 0.65em;
-    background-color: GetVariable('error');
-    border-radius: 50%;
-    cursor: pointer;
+    flex-direction: column;
+    gap: 0.25rem;
+
+    position: absolute;
+    top: -5px;
+    right: -5px;
+
     transition: opacity 150ms ease-out, transform 150ms ease-out;
+
+    & > * {
+      aspect-ratio: 1 / 1;
+      height: 22px;
+      color: white;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 0.65em;
+      border-radius: 50%;
+      cursor: pointer;
+    }
 
     @media (hover: hover) {
       opacity: 0;
       transform: translateY(50%);
     }
+
+    &:hover {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+
+  &__delete {
+    background-color: GetVariable('error');
+  }
+
+  &__download {
+    background-color: GetVariable('primary');
   }
 
   &:hover {
-    .kvass-media-thumbnail--delete {
+    .kvass-media-thumbnail__actions {
       opacity: 1;
       transform: translateY(0);
     }
